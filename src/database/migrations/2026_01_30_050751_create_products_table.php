639d00b930b008ee8234b71cb1cbf3e7ee3a1ff9
yuki <raiku.yuki@icloud.com>
2026-01-30 06:21:17 +0900

    productsテーブル作成とダミーデータ投入準備完了

---
+++ src/database/migrations/2026_01_30_050751_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');      // 商品名
+            $table->integer('price');    // 価格
+            $table->string('image');     // 画像ファイル名
+            $table->text('description')->nullable(); // 説明文（詳細用）
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
